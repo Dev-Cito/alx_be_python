1fcee62f1a2b17453ee8ee51fa98e9ef962b1331
Francois Mpangirwa <129337438+LeshitOR@users.noreply.github.com>
2025-07-19 17:02:11 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,29 +1,22 @@
 
-Task = input("Enter your task:")
-Priority = input("Priority (high/medium/low):").lower()
-Time_bound = input("Is it time-bound? (yes/no):").lower()
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-match Priority:
+match priority:
     case "high":
-        message = f"'{Task}' is a High-priority task"
+        reminder = f"'{task}' is a High-priority task"
     case "medium":
-        message = f"'{Task}' is a Medium-priority task"
+        reminder = f"'{task}' is a Medium-priority task"
     case "low":
-        message = f"'{Task}' is a Low-priority task."
+        reminder = f"'{task}' is a Low-priority task"
     case _:
-        message = f"'{Task}' is a Unknown-priority task"
+        reminder = f"'{task}' is a Unknown-priority task"
 
-if Time_bound == "yes":
-    message += " that requires immediate attention today!"
-    print("\nReminder:", end=" ")
-    print(message)
-elif Time_bound == "no":
-    message += " Consider completing it when you have free time."
-    print("\nNote:", end=" ")
-    print(message)
-else :
-    print("Invalid time bound!")
+if time_bound == "yes":
+    reminder += " that requires immediate attention today!"
+else:
+    reminder += " that you can work on when convenient."
 
-
-
-
+print("\n--- Daily Reminder ---")
+print(reminder)
