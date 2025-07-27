1e62f35e9593ea2ad96462b02ba20d94be00bb3e
Francois Mpangirwa <129337438+Dev-Cito@users.noreply.github.com>
2025-07-27 13:42:03 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -3,36 +3,42 @@ from re import S
 
 
 def display_menu():
-    print("Shopping List Manager")
+    print("\nShopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
-    print("4. Exit")
+    print("4. Exit\n")
+    print("=" * 30)
+
 
 def main():
     shopping_list = []
+
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = float(input("Enter your choice: "))
 
-        if choice == '1':
+        if choice == 1:
             # Prompt for and add an item
             add_item = str(input("Enter the item to add:"))
-
             shopping_list.append(add_item)
             pass
-        elif choice == '2':
+
+        elif choice == 2:
             # Prompt for and remove an item
-            remove_item = str(input("Remove an item:"))
+            remove_item = str(input("Remove the item:"))
             shopping_list.remove(remove_item)
             pass
-        elif choice == '3':
+
+        elif choice == 3:
             # Display the shopping list
             print(shopping_list)
             pass
-        elif choice == '4':
+
+        elif choice == 4:
             print("Goodbye!")
             break
+
         else:
             print("Invalid choice. Please try again.")
 
