662169c192e1fbfaaf2c4beef45f9bb623898306
Francois Mpangirwa <129337438+Dev-Cito@users.noreply.github.com>
2025-07-27 13:43:22 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -3,13 +3,11 @@ from re import S
 
 
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
-    print("4. Exit\n")
-    print("=" * 30)
-
+    print("4. Exit")
 
 def main():
     shopping_list = []
