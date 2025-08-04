3019d8f4546dce284ac913f5d97e08528b0d8e83
Francois Mpangirwa <129337438+Dev-Cito@users.noreply.github.com>
2025-08-04 19:04:36 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,54 +1,27 @@
-
-
-class BankAccount :
-    def __init__(self, account_balance = 0) :
-        self.account_balance = account_balance
-
-    def deposit(self, amount):
-        self.account_balance += amount
-    def withdraw(self, amount):
-        if amount > self.account_balance :
-            return False
-        elif amount < self.account_balance :
-            return True
-            self.account_balance -= amount
-        else :
-            print("Something went wrong!")
-    def display_balance(self):
-        return f"Current Balance:{self.account_balance}"
-
-    
-my_account = BankAccount()
-print(my_account.deposit(200))
-print(my_account.withdraw(10))
-print(my_account.display_balance())
-
-
-
-
 class BankAccount:
-    def __init__(self, account_balance=0):
-        self.account_balance = account_balance
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
 
     def deposit(self, amount):
-        self.account_balance += amount
-        return f"Deposited {amount} successfully."
+        if amount > 0:
+            self.__account_balance += amount
+            return f"Deposited: ${amount:.1f}"
+        else:
+            return "Deposit amount must be positive."
 
     def withdraw(self, amount):
-        if amount > self.account_balance:
-            return "Insufficient balance."
-        else:
-            self.account_balance -= amount
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
             return f"Withdrew {amount} successfully."
+        else:
+            return "Insufficient funds."
 
     def display_balance(self):
-        return f"Current balance: {self.account_balance}"
-
-
+        return f"Current balance: {self.__account_balance}"
+        
 my_account = BankAccount()
 
 print(my_account.deposit(200))
 print(my_account.withdraw(100))
 print(my_account.display_balance())
-
 
